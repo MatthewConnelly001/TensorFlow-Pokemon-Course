76b7e3d9b8b60f634f77d6301a5a6de3182efbd1
MatthewConnelly001 <42534260+MatthewConnelly001@users.noreply.github.com>
2020-10-08 15:11:02 +1100

    added code

--- code.py
+++ code.py
@@ -1,11 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+@author: MattC
+"""
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import preprocessing
-# NOTE: Modules in Anaconda can only open if you open VSCode via Anaconda/Anaconda prompt (type "code")
-# OR perhaps just use this? & C:/Users/MattC/Anaconda3/python.exe c:/Users/MattC/Documents/GitHub/TensorFlow-Pokemon-Course/code.py
+import random
+# NOTE: Anaconda modeules can only apply if you open VSCode via Anaconda prompt (type "code")
+
+###
+### COLLECTING DATA
+###
 
 # Extracting data from CSV and limiting it to columns we care about...
 df = pd.read_csv('pokemon.csv')
@@ -25,4 +33,65 @@ def dummy_creation(df, dummy_categories):
     return(df)
 
 df = dummy_creation(df, ['Egg_Group_1', 'Body_Style', 'Color','Type_1', 'Type_2'])
+
+###
+### SPLITTING AND NORMALIZING DATA
+###
+
+# Splits into training and seperating a specific column for testing 
+def train_test_splitter(DataFrame, column):
+    df_train = DataFrame.loc[df[column] != 1]
+    df_test = DataFrame.loc[df[column] == 1]
+    df_train = df_train.drop(column, axis=1)
+    df_test = df_test.drop(column, axis=1)
+    return(df_train, df_test)
+
+df_train, df_test = train_test_splitter(df, 'Generation')
+
+# creating labels for the expected answer of each set
+def label_delineator(df_train, df_test, label):
+    train_data = df_train.drop(label, axis=1).values
+    train_labels = df_train[label].values
+    test_data = df_test.drop(label,axis=1).values
+    test_labels = df_test[label].values
+    return(train_data, train_labels, test_data, test_labels)
+
+train_data, train_labels, test_data, test_labels = label_delineator(df_train, df_test, 'isLegendary')
+
+# normalizing all values (mean=0, s.d.=1)
+def data_normalizer(train_data, test_data):
+    train_data = preprocessing.MinMaxScaler().fit_transform(train_data)
+    test_data = preprocessing.MinMaxScaler().fit_transform(test_data)
+    return(train_data, test_data)
+
+train_data, test_data = data_normalizer(train_data, test_data)
+
+###
+### CREATE THE MODEL
+###
+length = train_data.shape[1]
+
+model = keras.Sequential()
+model.add(keras.layers.Dense(500, activation='relu', input_shape=[length,]))
+model.add(keras.layers.Dense(2, activation='softmax')) # Softmax is a type of logistic regression done for situations with multiple cases
+
+###
+### COMPILE AND EVALUATE THE MODEL
+###
+model.compile(optimizer='sgd', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.fit(train_data, train_labels, epochs=400)
+
+loss_value, accuracy_value = model.evaluate(test_data, test_labels)
+print(f'Our test accuracy was {accuracy_value}')
+
+
+def predictor(test_data, test_labels, index):
+    prediction = model.predict(test_data)
+    if np.argmax(prediction[index]) == test_labels[index]:
+        print(f'Index \"{index}\" was correctly predicted to be a \"{test_labels[index]}\"!')
+    else:
+        print(f'Index \"{index}\" was incorrectly predicted to be a \"{np.argmax(prediction[index])}\". It was actually a \"{test_labels[index]}\".')
+        return(prediction)
+    
+predictor(test_data, test_labels, random.randint(0, len(test_data)-1))
 
